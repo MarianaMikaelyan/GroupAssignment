b65b3006efe955d53511564a37c33680bc6f7197
Keti Davitadze <ketidavitadze2005@gmail.com>
2024-05-01 16:11:18 +0400

    Re-implemented the user credential feature / fixed the repo history bug

--- CreateAccount.py
+++ CreateAccount.py
@@ -1,9 +1,8 @@
 import random
 import string
 
-
-from database import *
-
+database = {}
+__all__ = ['database']
 
 def validate_account_number():
     prefix = 'TB'
@@ -27,5 +26,23 @@ def register_user():
         balance = 0
 
 
-if __name__ == "__main__":
-    register_user()
+register_user()
+
+
+def add_funds(account_number, amount, accounts):
+    if account_number in accounts:
+        if isinstance(amount, (int, float)) and amount > 0:
+            accounts[account_number]['Balance'] += amount
+            print(f"Balance was filled with {amount} GEL. New balance: {accounts[account_number]['Balance']} GEL")
+            return True
+        else:
+            print("Invalid amount entered. Please enter a positive number.")
+    else:
+        print("Invalid account number. Please enter a valid account number.")
+    return False
+
+
+account_number = input("Enter your account number: ")
+top_up_amount = float(input("Enter the amount to top up: "))
+
+add_funds(account_number, top_up_amount, database)
