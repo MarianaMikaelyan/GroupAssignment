bc0a60e07a534f81b8e350679386e5d697bc73c7
MarianaMikaelyan <167805522+MarianaMikaelyan@users.noreply.github.com>
2024-05-02 23:51:28 +0400

    Update MoneyTransfer.py
    Update

--- MoneyTransfer.py
+++ MoneyTransfer.py
@@ -1,26 +1,22 @@
-
-
 from CreateAccount import database
+from AccountHistory import save_transaction 
 
-def transaction(sender_account, receiver_account, amount, accounts):
-    if sender_account in accounts:
-        if receiver_account in accounts:
-            if amount <= accounts[sender_account]['Balance']:
-                accounts[sender_account]['Balance'] -= amount
-                accounts[receiver_account]['Balance'] += amount
-                print("The transaction was completed successfully.", 
-                      accounts[sender_account]['First Name'], "-->", 
-                      accounts[receiver_account]['First Name'], ":", amount)
-            else:
-                print("Not Enough Money")
+def transaction(sender_account, receiver_account, amount):
+    if sender_account in database and receiver_account in database:
+        if database[sender_account]['Balance'] >= amount:
+            database[sender_account]['Balance'] -= amount
+            database[receiver_account]['Balance'] += amount
+            print(f"Transaction successful! {amount} GEL transferred from {sender_account} to {receiver_account}.")
+            save_transaction(sender_account, f"Sent: -{amount} GEL to {receiver_account}")
+            save_transaction(receiver_account, f"Received: +{amount} GEL from {sender_account}")
         else:
-            print("Receiver Not Found")
+            print("Insufficient balance.")
+            save_transaction(sender_account, f"Failed to send: -{amount} GEL to {receiver_account}")
     else:
-        print("User Not Found")
+        print("Invalid account number(s).")
 
 if __name__ == "__main__":
     sender_account = input("Enter Your Account Number: ")
     receiver_account = input("Enter The Account Number Of The Receiver: ")
     amount = float(input("Enter The Amount Of Money: "))
-
-    transaction(sender_account, receiver_account, amount, database)
+    transaction(sender_account, receiver_account, amount)
