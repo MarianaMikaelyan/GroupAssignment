a3a1462129bd72b0c29577aa1ff603bd40866e08
MarianaMikaelyan <167805522+MarianaMikaelyan@users.noreply.github.com>
2024-05-09 20:36:59 +0400

    Update MoneyTransfer.py
    Update MoneyTransfer.py

--- MoneyTransfer.py
+++ MoneyTransfer.py
@@ -1,4 +1,5 @@
-
+import csv
+from datetime import datetime
 from CreateAccount import database
 from AccountHistory import save_transaction 
 
@@ -7,20 +8,25 @@ def transaction(sender_account, receiver_account, amount):
         if database[sender_account]['Balance'] >= amount:
             database[sender_account]['Balance'] -= amount
             database[receiver_account]['Balance'] += amount
-            print(f"Transaction successful! {amount} GEL transferred from {sender_account} to {receiver_account}.")
-            save_transaction(sender_account, f"Sent: -{amount} GEL to {receiver_account}")
-            save_transaction(receiver_account, f"Received: +{amount} GEL from {sender_account}")
-            save_trans(sender_account, database[sender_account]['First Name'], database[sender_account]['Last Name'], amount, "Sent")
-            save_trans(receiver_account, database[receiver_account]['First Name'], database[receiver_account]['Last Name'], amount, "Received")
+            transaction_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"Transaction successful! {amount} GEL transferred from {sender_account} to {receiver_account} at {transaction_time}.")
+            save_transaction(sender_account, f"Sent: -{amount} GEL to {receiver_account} at {transaction_time}")
+            save_transaction(receiver_account, f"Received: +{amount} GEL from {sender_account} at {transaction_time}")
+            save_trans(sender_account, database[sender_account]['First Name'], database[sender_account]['Last Name'], amount, "Sent", transaction_time)
+            save_trans(receiver_account, database[receiver_account]['First Name'], database[receiver_account]['Last Name'], amount, "Received", transaction_time)
         else:
             print("Insufficient balance.")
             save_transaction(sender_account, f"Failed to send: -{amount} GEL to {receiver_account}")
     else:
         print("Invalid account number(s).")
 
-def save_trans(account_number, first_name, last_name, amount, transaction_type):
+def save_trans(account_number, first_name, last_name, amount, transaction_type, transaction_time):
+    with open("transactions.csv", mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([first_name, last_name, account_number, amount, transaction_type, transaction_time])
+
     with open("transactions.txt", "a") as file:
-        file.write(f"{first_name}, {last_name}, {account_number}, {amount}, {transaction_type}\n")
+        file.write(f"{first_name}, {last_name}, {account_number}, {amount}, {transaction_type}, {transaction_time}\n")
 
 if __name__ == "__main__":
     sender_account = input("Enter Your Account Number: ")
